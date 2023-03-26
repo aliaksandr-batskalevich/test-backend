fadbe751fdc168f001de49e0c97df48ddd29491c
”Aliaksandr-Batskalevich” <aliaksandr.batskalevich@gmail.com>
2023-03-26 14:32:36 +0300

    Test server
    PORT: 5050
    GET: /test

--- sevices/test.service.js
+++ sevices/test.service.js
@@ -1,8 +1,49 @@
-
 class TestService {
     async getTest() {
-        const result = {};
-        return {message: `Ok!`, data: {result}};
+        const users = [
+            {
+                id: 1,
+                username: 'Alex1',
+                isActivated: true,
+
+                gamesCount: 0,
+                gamesWinsCount: 0,
+                sparringCount: 0,
+                sparringWinsCount: 0,
+            },
+            {
+                id: 2,
+                username: 'Alex2',
+                isActivated: true,
+
+                gamesCount: 0,
+                gamesWinsCount: 0,
+                sparringCount: 0,
+                sparringWinsCount: 0,
+            },
+            {
+                id: 3,
+                username: 'Alex3',
+                isActivated: true,
+
+                gamesCount: 0,
+                gamesWinsCount: 0,
+                sparringCount: 0,
+                sparringWinsCount: 0,
+            },
+            {
+                id: 4,
+                username: 'Alex4',
+                isActivated: true,
+
+                gamesCount: 0,
+                gamesWinsCount: 0,
+                sparringCount: 0,
+                sparringWinsCount: 0,
+            },
+        ];
+        const totalCount = users.length;
+        return {message: `Success!`, data: {totalCount, users}};
     }
 }
 
